4ddc9bea84bc28c85965cccee091dbb7221e7ce6
Austin Reifsteck <apreifsteck@gmail.com>
2024-04-10 20:02:49 -0400

    first commit

---
+++ lib/summoner_tracker.ex
@@ -0,0 +1,18 @@
+defmodule SummonerTracker do
+  @moduledoc """
+  Documentation for `SummonerTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SummonerTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/summoner_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule SummonerTrackerTest do
+  use ExUnit.Case
+  doctest SummonerTracker
+
+  test "greets the world" do
+    assert SummonerTracker.hello() == :world
+  end
+end
